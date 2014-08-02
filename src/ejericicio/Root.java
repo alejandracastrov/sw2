24ac7b3b39119ea66528a1b25beccc73ba6cf338
Administrator <Administrator@SISTEMAS13.estudiantes.uco>
2014-08-01 20:08:59 -0500

    proyecto de software

---
+++ src/ejericicio/Root.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejericicio;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class Root {
+    
+    
+    
+     public static void main(String[] args) {
+           
+     Pagos p1= new Pagos();
+     Membresia mem=new Membresia();
+     Scanner leer= new Scanner(System.in);
+     Video vid=new Video();
+     Agente agen=new Agente();
+     
+     //boolean while1=true;
+     int op;
+     
+      // while(while1){
+        System.out.println("1: fisico");
+        System.out.println("2: libro");
+        System.out.println("3: membresia");
+        System.out.println("4: video");
+        System.out.println("ingrese la opción: ");
+        op=leer.nextInt();
+        switch(op){
+            case 1:
+              p1.generar_Orden();
+              agen.generar_pagodecomisión();
+              //while1=false;
+                break;
+            case 2:
+                p1.generar_Orden();
+                p1.enviarDept();
+                agen.generar_pagodecomisión();
+                break;
+            case 3:       
+                int op2;
+                System.out.println("1: Activar");
+                System.out.println("2: Actualizar"); 
+                op2=leer.nextInt();
+                switch(op2){
+                    case 1:
+                        mem.Activar_Membresia();
+                        mem.Enviar_Email(1);
+                        break;
+                    case 2:
+                        mem.Actualizar_Membresia();
+                        mem.Enviar_Email(2);
+                        break;
+                }
+                    
+                break;
+            case 4:
+                vid.comparar_video();
+                break;
+            default:
+                break;
+             } 
+         }
+     }
+
+         
+ 
